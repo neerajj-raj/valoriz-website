5c7a0ae791d13907d0fc2ce9f673d626cd51e896
Neeraj Raj <neeraj.r@valoriz.com>
2025-05-09 06:38:02 +0530

    delete all files

--- storefront/src/common/core/button/types.ts
+++
@@ -1,7 +0,0 @@
-import { VariantProps } from "class-variance-authority";
-import { ButtonHTMLAttributes } from "react";
-import { buttonCva } from "./Button";
-
-export type ButtonCVAProps = VariantProps<typeof buttonCva>;
-
-export type ButtonProps = { showLoading?: boolean } & ButtonCVAProps & ButtonHTMLAttributes<HTMLButtonElement>;
